931904d07ad83b3ea8be5670cca7791bc6a7952d
ant290 <ant290@live.co.uk>
2019-10-11 17:29:14 +0100

    Repeating views

---
+++ Beths Pies/Beths Pies/Adapters/PieAdapter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using BethanysPieShopMobile.Utility;
+using Beths_Pies.ViewHolders;
+using BethsPiesMobile.core.Models;
+using BethsPiesMobile.core.Repositories;
+
+namespace Beths_Pies.Adapters
+{
+    public class PieAdapter: RecyclerView.Adapter
+    {
+        private List<Pie> _pies;
+
+        public PieAdapter()
+        {
+            var pieRepository = new PieRepository();
+            _pies = pieRepository.GetAllPies();
+        }
+
+        public override int ItemCount => _pies.Count;
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            if (holder is PieViewHolder pieViewHolder)
+            {
+                pieViewHolder.PieNameTextView.Text = _pies[position].Name;
+
+                var imageBitmap = ImageHelper.GetImageBitmapFromUrl(_pies[position].ImageThumbnailUrl);
+                pieViewHolder.PieImageView.SetImageBitmap(imageBitmap);
+            }
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View itemView =
+                LayoutInflater.From(parent.Context).Inflate(Resource.Layout.Pie_viewHolder, parent, false);
+
+            PieViewHolder pieViewHolder = new PieViewHolder(itemView);
+            return pieViewHolder;
+        }
+
+        
+    }
+}
---
+++ Beths Pies/Beths Pies/PieMenuActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Beths_Pies.Adapters;
+
+namespace Beths_Pies
+{
+    [Activity(Label = "PieMenuActivity", MainLauncher = true)]
+    public class PieMenuActivity : Activity
+    {
+        private RecyclerView _pieRecyclerView;
+        private RecyclerView.LayoutManager _pieLayoutManager;
+        private PieAdapter _pieAdapter;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Pie_menu);
+            _pieRecyclerView = FindViewById<RecyclerView>(Resource.Id.pieMenuRecyclerView);
+
+            //layout manager
+            _pieLayoutManager = new LinearLayoutManager(this);
+            _pieRecyclerView.SetLayoutManager(_pieLayoutManager);
+
+            //pie adapter
+            _pieAdapter = new PieAdapter();
+            _pieRecyclerView.SetAdapter(_pieAdapter);
+
+            //view holder
+        }
+    }
+}
